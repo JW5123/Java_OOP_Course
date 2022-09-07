6bf064b5a57619f40166387beb7033da02b95861
JW5123 <Jay2002714@gmail.com>
2022-09-07 19:52:57 +0800

    Upload New File

---
+++ HW1/Class05.java
@@ -0,0 +1,31 @@
+class CBox{
+    int length;
+    int width;
+    int height;
+
+    int volume(){
+        return length * width * height;
+    }
+    int surfaceArea(){
+        return 2 * ((length * width) + (width * height) + (length * height));
+    }
+    void showData(){
+        System.out.printf("length = %d\n", length);
+        System.out.printf("width = %d\n", width);
+        System.out.printf("height = %d\n", height);
+    }
+    void showAll(){
+        showData();
+        System.out.printf("surfaceArea = %d\n", surfaceArea());
+        System.out.printf("volume = %d\n", volume());
+    }
+}
+public class Class05 {
+    public static void main(String[] args) {
+        CBox box = new CBox();
+        box.length = 1;
+        box.width = 1;
+        box.height = 1;
+        box.showAll();
+    }
+}
---
+++ HW1/Class13.java
@@ -0,0 +1,19 @@
+class CCircle{
+    double area(double r){
+        return r * r * 3.14;
+    }
+    float area(float r){
+        return r * r * 3.14f;
+    }
+    double area(int r){
+        return r * r * 3.14;
+    }
+}
+public class Class13 {
+    public static void main(String[] args) {
+        CCircle obj = new CCircle();
+        System.out.println("area(2) = " + obj.area(2));
+        System.out.println("area(2.2f) = " + obj.area(2.2f));
+        System.out.println("area(2.2) = " + obj.area(2.2));
+    }
+}
