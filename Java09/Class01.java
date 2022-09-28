7e68bcce8644efb02c5ffc3dae04fa47447425f0
JW5123 <Jay2002714@gmail.com>
2022-09-29 01:53:12 +0800

    Update file

--- Java09/Class01.java
+++ Java09/Class01.java
@@ -3,21 +3,24 @@ class CRectangle{
     int width;
     int height;
 
+    //problem a : Define CRectangle constructor, and with arguments are w、h
     CRectangle(int w, int h){
         width = w;
         height = h;
-    }//problem a
+    }
 
+    //problem b : Define CRectangle constructor, without arguments
     CRectangle(){
         width = 10;
         height = 8;
-    }//problem b
+    }
     /*
+    problem c : Define CRectangle constructor, using "this()" to set the width and height 
     CRectangle(){
         this(10, 8)
     }
-    problem c
     */
+    // Print the width and height
     void show(){
         System.out.println("width= " + width);
         System.out.println("height= " + height);
@@ -25,8 +28,10 @@ class CRectangle{
 }
 public class Class01 {
     public static void main(String[] args) {
+        //Create object rec1
         CRectangle rec1 = new CRectangle(2, 3);
         rec1.show();
+        //Create object rec2
         CRectangle res2 = new CRectangle();
         res2.show();
     }
