c3a5e307fe65225dda41d16a668baf334b3ec217
JW5123 <Jay2002714@gmail.com>
2022-12-21 11:11:33 +0800

    Update file

--- Java111Q2/Class02.java
+++ Java111Q2/Class02.java
@@ -2,7 +2,7 @@ import java.io.*;
 
 public class Class02 {
     public static void main(String[] args) throws IOException {
-        FileWriter fw = new FileWriter("/Users/renjiewang/Documents/GitHub/Java_OOP_Course/12_21_test/rand.txt");
+        FileWriter fw = new FileWriter("/Users/renjiewang/Documents/GitHub/Java_OOP_Course/Java111Q2/rand.txt");
         BufferedWriter bw = new BufferedWriter(fw);
 
         for(int i = 1; i <= 1000; i++){
